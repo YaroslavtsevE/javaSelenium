87c62e7e7616e94fbd7a9e9831c91f98adaaaebc
Evgeny <yaroslavtseveugene@gmail.com>
2024-07-04 22:07:00 +0700

    need fix Subjects, hobbies, dateOfBrth

--- src/test/java/portfolio/demoQa.java
+++ src/test/java/portfolio/demoQa.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.MalformedURLException;
 import java.time.Duration;
 
 public class demoQa {
@@ -22,41 +23,58 @@ public class demoQa {
 
     @BeforeEach
     public void setUp() {
-//        String url = "https://the-internet.herokuapp.com/";
+
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://demoqa.com/automation-practice-form");
         driver.manage().window().maximize();
-    }
-    @Test
-    public void checkAlerts() throws InterruptedException {
-//        driver.get("https://demoqa.com/automation-practice-form");
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10), Duration.ofMillis(250));
+    }
+
+    @Test
+    public void checkAlerts() throws InterruptedException, MalformedURLException {
+
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5), Duration.ofMillis(250));
 
         wait.until(ExpectedConditions.elementToBeClickable(By.id("submit")));
 
         WebElement submitBtn = driver.findElement(By.id("submit"));
         submitBtn.submit();
-//        Actions actions = new Actions(driver);
-//        actions.moveToElement(submitBtn).click().build().perform();
-
         WebElement userForm = driver.findElement(By.id("userForm"));
         Assertions.assertEquals("was-validated", userForm.getAttribute("class"));
-//        driver.findElement(By.id("submit")).click();
-//        driver.findElement(By.className(".was-validated")).isDisplayed();
 
         driver.findElement(By.cssSelector("[placeholder='First Name']")).sendKeys("Evgeny");
         driver.findElement(By.cssSelector("[placeholder='Last Name']")).sendKeys("Yaroslavtsev");
         driver.findElement(By.cssSelector("[placeholder='name@example.com']")).sendKeys("ye@ya.ru");
-//
+
         WebElement genderCheckBox = driver.findElement(By.id("gender-radio-1"));
-        Actions actions = new Actions(driver);
         actions.moveToElement(genderCheckBox).click().build().perform();
-//
         Assertions.assertEquals("true", genderCheckBox.getAttribute("checked"));
+
+        driver.findElement(By.id("userNumber")).sendKeys("1234567890As&ы");
+
+        driver.findElement(By.id("dateOfBirthInput")).clear();
+        driver.findElement(By.id("dateOfBirthInput")).sendKeys("24 Dec 1991");
+
+        driver.findElement(By.id("subjectsInput")).sendKeys("Maths");
+        driver.findElement(By.id("subjectsInput")).sendKeys("Chemistry");
+
+        WebElement hobbiesCheckbox1 = driver.findElement(By.id("hobbies-checkbox-1"));
+        actions.moveToElement(hobbiesCheckbox1).click().build().perform();
+
+
+        String filePath = "C:\\Users\\Admin\\Downloads\\photo_2024-03-21_19-24-07.jpg";
+        driver.findElement(By.id("uploadPicture")).sendKeys(filePath);
+
+        submitBtn.submit();
+//        Assertions.assertEquals("true", hobbiesCheckbox1.getAttribute("checked"));
+
+//        WebElement inputF = driver.findElement(By.id(“dateOfBirth”));
+//
+//        WebElement selectDropdown = driver.findElement(By.className(".react-datepicker__month-select"));
+//        Select select = new Select(selectDropdown);
+//        select.selectByVisibleText("December");
 
 //        WebElement genderCheckBox = driver.findElement(By.cssSelector("[id='gender-radio-1']"));
 //        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id='gender-radio-1']")));
